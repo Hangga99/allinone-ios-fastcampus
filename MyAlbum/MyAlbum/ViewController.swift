0aa4f45700e35328f7644ed7f94025fc9363108d
Hangga99 <qkrrlxo418@naver.com>
2022-10-15 00:17:59 +0900

    First Commit

---
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 박기태 on 2022/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
