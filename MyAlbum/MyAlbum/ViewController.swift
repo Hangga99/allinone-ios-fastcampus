5d903021eacea58652926913e0ec6c3b15c7f572
Hangga99 <qkrrlxo418@naver.com>
2022-10-16 01:09:49 +0900

    UpDate

--- MyAlbum/MyAlbum/ViewController.swift
+++ MyAlbum/MyAlbum/ViewController.swift
@@ -1,19 +1,17 @@
-//
-//  ViewController.swift
-//  MyAlbum
-//
-//  Created by 박기태 on 2022/10/15.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let alert = UIAlertController(title: "Bye", message: "잘가세요!!", preferredStyle: .alert) // UIAlertController으로 팝업을 만듬
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action) // Button은 UIAlertAction을 가지고 만들고
+        present(alert, animated: true, completion: nil) // present을 통해서 알럿을 띄운다
+    }
+    
 }
 
